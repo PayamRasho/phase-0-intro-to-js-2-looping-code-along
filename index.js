190782600631c6bf28caa1a6bdfca3f16b2db7a9
PayamRasho <payamrfattah01@gmail.com>
2024-02-21 22:01:08 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+function wrapGifts(myArray) {
+  for (let i = 0; i < myArray.length; i++) {
+    console.log(`Wrapped ${myArray[0]} and added a bow!`);
+  }
+}
+
+function writeCards(myArray, event) {
+  let newaraay = [];
+  for (let i = 0; i < myArray.length; i++) {
+    newaraay.push(`Thank you, ${myArray[i]}, for the wonderful ${event} gift!`);
+  }
+  return newaraay;
+}
+
+console.log(writeCards(["Payam", "Muhammad", "Peshraw"], "birthday"));
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
